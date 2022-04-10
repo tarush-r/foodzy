7086e92b43339c5f6dc81e380c6dae80e0cf643e
Anina Pillai <aninanair99512@gmail.com>
2022-04-10 09:57:43 +0530

    Ingredient substitute

---
+++ lib/screens/joke_trivia.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JokeTrivia extends StatefulWidget {
+  @override
+  _JokeTriviaState createState() => _JokeTriviaState();
+}
+
+class _JokeTriviaState extends State<JokeTrivia> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
